9128a396005585532ef8818d9fdac9d16b9aeb34
Alina Garaeva <crazyalin91@mail.ru>
2022-03-06 12:26:33 +0300

    Init

---
+++ NetCoreKafkaDemo/NetCoreKafkaDemo/Program.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace NetCoreKafka
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CreateHostBuilder(args).Build().Run();
+        }
+
+        private static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+            .ConfigureServices((context, collection) =>
+            {
+                collection.AddHostedService<KafkaConsumerHostedService>();
+                collection.AddHostedService<KafkaProducerHostedServices>();
+            });
+
+    }
+}
